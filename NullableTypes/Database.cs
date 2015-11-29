324639d975fdb4b710defbacb3fa5e8c82ccf6b4
JesseDietrichson <jesse.dietrichson@xamarin.com>
2015-11-28 21:17:56 -0500

    Initial Commit

---
+++ NullableTypes/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    //A class to simulate reading a database
+    class Database
+    {
+        public int? GetUsersAge(string user)
+        {
+            //Lets pretend the user does not exist and because of that we now can't get the age
+            return null;
+        }
+    }
+}
